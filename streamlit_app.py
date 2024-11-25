6bde90a0008a08d692c24a626007b9d98aa07932
zlsfdabw <zlayer.trask@gmail.com>
2024-11-25 13:37:22 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,27 +20,12 @@ st.dataframe(data=my_dataframe, use_container_width=True)
 
 # Convert the Snowpark Dataframe to a Pandas Dataframe so we can use the LOC function
 df = my_dataframe.to_pandas()
-st.dataframe(df)
-st.stop()
-
-# cnx = st.connection("snowflake")
-# # session = cnx.session()
-# my_dataframe = cnx.query("select * from zenas_athleisure_db.products.catalog_for_website")
-# df = my_dataframe.to_pandas()
 # st.dataframe(df)
-
-# st.stop()
-
-# my_dataframe = session.table("zenas_athleisure_db.products.catalog_for_website").select(col('color_or_style'),col('price'))
-# st.dataframe(data=my_dataframe, use_container_width=True)
-# st.stop()
-
-# Convert the Snowpark Dataframe to a Pandas Dataframe so we can use the LOC function
-# pd_df = my_dataframe.to_pandas()
-# st.dataframe(pd_df)
 # st.stop()
 
 sweatsuit_picker = st.selectbox(
     'Pick a sweatsuit color or style:'
-    ,my_dataframe
+    ,df.color_or_style
 )
+
+st.stop()
