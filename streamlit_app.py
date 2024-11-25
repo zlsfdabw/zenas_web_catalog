df2c199bb2dfab03bb2ff26e09b893d13c9cbff9
zlsfdabw <zlayer.trask@gmail.com>
2024-11-25 13:34:08 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,6 +12,8 @@ st.title("Zena's Amazing Athleisure Catalog")
 # name_on_order = st.text_input('Name on Smoothie:')
 # st.write('The name on your Smoothie will be:', name_on_order)
 
+cnx = st.connection("snowflake")
+session = cnx.session()
 my_dataframe = session.table("zenas_athleisure_db.products.catalog")
 st.dataframe(data=my_dataframe, use_container_width=True)
 # st.stop()
