88a6bd54f842f575230bc622a636834f35f2eb36
zlsfdabw <zlayer.trask@gmail.com>
2024-11-25 13:29:56 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,13 +12,22 @@ st.title("Zena's Amazing Athleisure Catalog")
 # name_on_order = st.text_input('Name on Smoothie:')
 # st.write('The name on your Smoothie will be:', name_on_order)
 
-cnx = st.connection("snowflake")
-# session = cnx.session()
-my_dataframe = cnx.query("select * from zenas_athleisure_db.products.catalog_for_website")
+my_dataframe = session.table("zenas_athleisure_db.products.catalog")
+st.dataframe(data=my_dataframe, use_container_width=True)
+# st.stop()
+
+# Convert the Snowpark Dataframe to a Pandas Dataframe so we can use the LOC function
 df = my_dataframe.to_pandas()
 st.dataframe(df)
-
 st.stop()
+
+# cnx = st.connection("snowflake")
+# # session = cnx.session()
+# my_dataframe = cnx.query("select * from zenas_athleisure_db.products.catalog_for_website")
+# df = my_dataframe.to_pandas()
+# st.dataframe(df)
+
+# st.stop()
 
 # my_dataframe = session.table("zenas_athleisure_db.products.catalog_for_website").select(col('color_or_style'),col('price'))
 # st.dataframe(data=my_dataframe, use_container_width=True)
