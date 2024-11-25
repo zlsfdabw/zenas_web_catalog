0a3020c232afd8b4bd341576b3758f6ed5d53996
zlsfdabw <zlayer.trask@gmail.com>
2024-11-25 12:41:20 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -39,28 +39,31 @@ sweatsuit_picker = st.selectbox(
 #     ,max_selections=5
 # )
 
-if ingredients_list:
-    ingredients_string = ''
-
-    for fruit_chosen in ingredients_list:
-        ingredients_string += fruit_chosen + ' '
-
-        search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
-        # st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
-
-        st.subheader(fruit_chosen + ' Nutrition Information')
-        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + search_on)
-        sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
 
-    #st.write(ingredients_string)
+    
+# if ingredients_list:
+#     ingredients_string = ''
 
-    my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
-        values ('""" + ingredients_string + """','""" + name_on_order + """')"""
+#     for fruit_chosen in ingredients_list:
+#         ingredients_string += fruit_chosen + ' '
 
-    #st.write(my_insert_stmt)
-    time_to_insert = st.button('Submit Order')
+#         search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+#         # st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
 
-    if time_to_insert:
-        session.sql(my_insert_stmt).collect()
-        st.success('Your Smoothie is ordered, ' + name_on_order + '!' , icon="✅")
+#         st.subheader(fruit_chosen + ' Nutrition Information')
+#         smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + search_on)
+#         sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
+
+
+#     #st.write(ingredients_string)
+
+#     my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
+#         values ('""" + ingredients_string + """','""" + name_on_order + """')"""
+
+#     #st.write(my_insert_stmt)
+#     time_to_insert = st.button('Submit Order')
+
+#     if time_to_insert:
+#         session.sql(my_insert_stmt).collect()
+#         st.success('Your Smoothie is ordered, ' + name_on_order + '!' , icon="✅")
