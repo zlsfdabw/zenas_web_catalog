cde83cdb0b8a6cdf0963bc2e09200a74e6343813
zlsfdabw <zlayer.trask@gmail.com>
2024-11-25 13:39:47 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,13 +19,13 @@ st.dataframe(data=my_dataframe, use_container_width=True)
 # st.stop()
 
 # Convert the Snowpark Dataframe to a Pandas Dataframe so we can use the LOC function
-df = my_dataframe.to_pandas()
-st.dataframe(df)
+# df = my_dataframe.to_pandas()
+# st.dataframe(df)
 # st.stop()
 
 sweatsuit_picker = st.selectbox(
     'Pick a sweatsuit color or style:'
-    ,df.color_or_style
+    ,my_dataframe.color_or_style
 )
 
 st.stop()
